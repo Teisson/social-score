eadb5eb138cf0ad95112960d1d7bee0a4c798b6b
NoobDeveloper412 <mzuhair412@gmail.com>
2024-04-14 20:09:05 +0500

    Updated styles, made some responsive updates and added the dynamic guage component.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,14 @@
 <script>
   // import CanvasTryout from '$lib/component/CanvasTryout.svelte';
-  
-    import SocialScorePage from '$lib/component/SocialScorePage.svelte';
-  import '../app.css'
-  
-  </script>
-  <SocialScorePage> </SocialScorePage>
-  <style>
-  
-   
-  </style>
+
+  import SocialScorePage from "$lib/component/SocialScorePage.svelte";
+  import "../app.css";
+  import "../fonts.css";
+
+  import "../lib/component/SocialScorePage.css";
+</script>
+
+<SocialScorePage></SocialScorePage>
+
+<style>
+</style>
